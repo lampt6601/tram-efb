ee86f6b096f2a18c86813a1532d0e4d3c864acd1
huucanh2k <tranhuucanh2000@gmail.com>
2026-03-14 21:25:27 +0700

    chore: Update shop image from PNG to JPG format and corresponding reference.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     url: "/",
     images: [
       {
-        url: "/thc-shop.png",
+        url: "/thc-shop.jpg",
         width: 1200,
         height: 630,
         alt: "THC eFootball Shop",
